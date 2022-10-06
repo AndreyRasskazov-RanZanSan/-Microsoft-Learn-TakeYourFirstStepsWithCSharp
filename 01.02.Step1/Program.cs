e91cfb60c55a95a52c390280e45dde5646afbbdc
Rasskazov Andrey <ranzansan@gmail.com>
2022-10-06 20:54:40 +0300

    Добавьте файлы проекта.

---
+++ 01.02.Step1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._02.Step1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {}
+    }
+}
